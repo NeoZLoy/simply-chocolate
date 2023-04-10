aab7295ace5962f8d800028f30637774b9da3951
NeoZLoy <123673700+NeoZLoy@users.noreply.github.com>
2023-04-10 12:03:08 +0200

    add video player

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 import './scripts/mobile-menu';
 import './scripts/modal';
+import './scripts/video';
---
+++ src/scripts/video.js
@@ -0,0 +1,4 @@
+// Інінціалізація програвача
+const player = new Plyr('#how-its-made-player');
+// Шлях до постера для програвача
+player.poster = '../img/how-its-made-desktop.jpg';
