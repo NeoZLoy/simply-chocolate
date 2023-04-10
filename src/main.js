4e81e71e182011748528f2ab7801b338fd90b04a
NeoZLoy <123673700+NeoZLoy@users.noreply.github.com>
2023-04-10 17:22:20 +0200

    update swiper

--- src/main.js
+++ src/main.js
@@ -2,4 +2,3 @@ import './scripts/mobile-menu';
 import './scripts/modal';
 import './scripts/video';
 import './scripts/swiper';
-import './scripts/swiper-mobile';
--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,9 +1,22 @@
 var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 3,
-
-  spaceBetween: 28,
+  slidesPerView: 1,
+  spaceBetween: 10,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
   },
 });
